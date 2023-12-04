334ff13ff045f90704ca405423ee078a8599dbfc
alierensofu <111981466+alierensofu@users.noreply.github.com>
2023-12-04 18:48:19 +0300

    string methodları
    visual studioda c# la udemyde verilen ödevdir

---
+++ string methods/string methods/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("please enter your name and press enter");
+string s1 = "";
+s1 = Console.ReadLine();
+string converts1_1 = s1.ToUpper();
+Console.WriteLine(converts1_1);
+string convertss1_2 = s1.ToLower();
+Console.WriteLine(convertss1_2);
+Console.WriteLine(@s1);
+string convertss1_3 = s1.Substring(4);
+Console.WriteLine(convertss1_3);
